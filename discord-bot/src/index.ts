2f4b8fc07f6e43c5437aae3000fab4c78a420d56
William Jackson <william@jaackson.com>
2025-03-11 02:21:37 +1000

    welcome message DM

--- discord-bot/src/index.ts
+++ discord-bot/src/index.ts
@@ -1,16 +1,17 @@
 import { ActionRowBuilder, ButtonBuilder } from '@discordjs/builders';
-import { ButtonStyle, Client, GatewayIntentBits } from 'discord.js';
+import { ButtonStyle, Client, Events, GatewayIntentBits } from 'discord.js';
 import dotenv from 'dotenv';
 import { startDispatcher } from './dispatch';
 import { redisClient } from './lib/redis';
+import config from '../config.json';
 
 dotenv.config();
 
 const client = new Client({ 
-    intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent ],
+    intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent ],
 });
 
-client.once('ready', async () => {
+client.once(Events.ClientReady, async () => {
     console.log(`Client is ready! Logged in as ${client.user?.tag}`);
     await redisClient.connect().catch(err => {
         console.error('Failed to connect to Redis:', err);
@@ -33,6 +34,18 @@ client.on('messageCreate', async (message) => {
         });
     }
 })
+
+client.on(Events.GuildMemberAdd, async (member) => {
+    await member.send({
+        content: `Thanks for joining! Before you can do anything you need to connect your __Griffith sNumber__.`,
+        components: [new ActionRowBuilder<ButtonBuilder>().addComponents([
+            new ButtonBuilder()
+                .setLabel('Go to Connect')
+                .setStyle(ButtonStyle.Link)
+                .setURL(config.connectMessageLink),
+        ])],
+    })
+});
 
 
 client.login(process.env.DISCORD_TOKEN);
